602dfc59fc2fe3c218337a989c6fd73241022b92
Guillaume <guillaume@jweb-creation.fr>
2016-08-31 15:12:17 +0200

    User

---
+++ App/Kernel/Common/User.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Kernel\Common;
+
+class User
+{
+    /* ************************************************** */
+    /* ****************   VARIABLES   ******************* */
+    /* ************************************************** */
+
+    protected static $instance = NULL ;
+
+    /* ************************************************** */
+    /* ****************   FUNCTIONS   ******************* */
+    /* ************************************************** */
+
+    protected function getNewToken()
+    {
+        $length = 32 ;
+        $uniq   = false ;
+
+        while( $uniq == false )
+        {
+            if ( function_exists('mcrypt_create_iv') )
+            {
+                $token = bin2hex(mcrypt_create_iv( $length , MCRYPT_DEV_URANDOM ) );
+            }
+            else if ( function_exists('random_bytes') )
+            {
+                $token = bin2hex( random_bytes( $length ) );
+            }
+            else if ( function_exists('openssl_random_pseudo_bytes') )
+            {
+                $token = bin2hex( openssl_random_pseudo_bytes( $length ) );
+            }
+
+            $uniq = $this->uniqToken( $token ) ;
+        }
+
+        return $token ;
+    }
+}
