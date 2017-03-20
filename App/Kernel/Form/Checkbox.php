00143ef49285a64255586bbebaea4c9bead508d7
Jammye <guillaume@jweb-creation.fr>
2017-03-20 08:22:17 +0100

    Update

--- App/Kernel/Form/Checkbox.php
+++ App/Kernel/Form/Checkbox.php
@@ -7,7 +7,9 @@ class Checkbox extends \App\Kernel\Back\Form
 	public function html( $field, $name, $value = NULL )
 	{
 		$this->initLib() ;
-		return $this->getMultiselect( $field, $name, $value ) ;
+
+		if ( $field->isParent() == true )   return $this->getMultiselectParent( $field , $name , $value ) ;
+		else                                return $this->getMultiselect( $field , $name , $value ) ;
 	}
 
     private function getMultiselect( $field, $name, $value = NULL )
@@ -15,7 +17,29 @@ class Checkbox extends \App\Kernel\Back\Form
         $this->_lib_js  = 'bootstrap-multiselect/dist/js/bootstrap-multiselect.js';
         $this->_lib_css = 'bootstrap-multiselect/dist/css/bootstrap-multiselect.css';
 
-        if ( $value === NULL ) $value = array();
+        if ( $value === NULL ) $value = [];
+
+        $select = '' ;
+        foreach( $field->getData('option') as $key => $opt )
+        {
+            $select .= '<option value="' . $key . '"' . ( in_array( $key , $value ) ? " selected" : '' ) . '>' . $opt . '</option>' . "\n" ;
+        }
+
+        return '
+        <div class="input-group btn-group">
+            <span class="input-group-addon">
+                    <i class="fa fa-th-list"></i>
+            </span>
+            <select name="' . $name . '" id="id_' . $field->getColumn() . '" data-plugin-multiselect multiple="multiple" data-live-search="true">
+                ' . $select . '
+            </select>
+        </div>' ;
+    }
+
+    private function getMultiselectParent( $field, $name, $value = NULL )
+    {
+
+        if ( $value === NULL ) $value = [];
 
         $select = '' ;
         foreach( $field->getData('option') as $key => $opt )
@@ -58,6 +82,6 @@ class Checkbox extends \App\Kernel\Back\Form
                 ' . $select . '
             </select>
         </div>' ;
-        }
+    }
 
 }
