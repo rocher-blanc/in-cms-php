a6414584141975632d0465ebbc426244b085d091
Jammye <guillaume@jweb-creation.fr>
2017-04-26 11:31:39 +0200

    Patch domaine dans le fichier de configuration

--- App/config/config.php
+++ App/config/config.php
@@ -31,13 +31,19 @@ defined('IMAGE_PATH') || define('IMAGE_PATH', WEB_PATH . '/images');
 defined('DOCUMENT_PATH') || define('DOCUMENT_PATH', WEB_PATH . '/documents');
 defined('UPLOAD_PATH') || define('UPLOAD_PATH', WEB_PATH . '/uploads');
 
-$configFileProject       = PROJECT_PATH . '/config/config.php' ;
-$configFileProjectDomain = PROJECT_PATH . '/config/config.' . $_SERVER['HTTP_HOST'] . '.php' ;
+$configFileProject          = PROJECT_PATH . '/config/config.php' ;
+$configFileProjectDomain    = PROJECT_PATH . '/config/config.' . $_SERVER['HTTP_HOST'] . '.php' ;
+$configFileProjectDomainWww = PROJECT_PATH . '/config/config.www.' . $_SERVER['HTTP_HOST'] . '.php' ;
 
 if ( file_exists( $configFileProject ) )
 {
     defined('FILE_CONFIG') || define('FILE_CONFIG',true);
     require $configFileProject ;
+}
+else if ( file_exists( $configFileProjectDomainWww ) )
+{
+    defined('FILE_CONFIG') || define('FILE_CONFIG',true);
+    require $configFileProjectDomainWww ;
 }
 else if ( file_exists( $configFileProjectDomain ) )
 {
