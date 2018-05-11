be576e5a85aec1aaf568183b76196781101c9ee9
Jammye <guillaume@jweb-creation.fr>
2018-05-11 21:50:17 +0200

    Update templates

--- App/config/config.back.php
+++ App/config/config.back.php
@@ -4,8 +4,8 @@ defined('CONTROLLERS_PATH') || define('CONTROLLERS_PATH', APPLICATION_PATH . '/C
 defined('PROJECT_CONTROLLER_PATH') || define('PROJECT_CONTROLLER_PATH', MODULE_PATH . '/Controller/Back');
 defined('PROJECT_EXT_CONTROLLER_PATH') || define('PROJECT_EXT_CONTROLLER_PATH', PROJECT_PATH . '/Controller/Back');
 defined('CACHE_PATH') || define('CACHE_PATH', _PATH_ . '/cache/back');
-defined('TEMPLATES_PATH') || define('TEMPLATES_PATH', APPLICATION_PATH . '/view/back/canvas');
-defined('VIEW_PROJECT_PATH') || define('VIEW_PROJECT_PATH', PROJECT_PATH . '/view/back/canvas');
+defined('TEMPLATES_PATH') || define('TEMPLATES_PATH', APPLICATION_PATH . '/view/back');
+defined('VIEW_PROJECT_PATH') || define('VIEW_PROJECT_PATH', PROJECT_PATH . '/view/back');
 defined('FORM_PATH') || define('FORM_PATH', KERNEL_PATH . '/Form');
 defined('MIDDLEWARE_PROJECT_PATH') || define('MIDDLEWARE_PROJECT_PATH', PROJECT_PATH . '/Middleware/Back');
 defined('ASSETS_IMG_PATH') || define('ASSETS_IMG_PATH', WEB_PATH . '/' . \App\Kernel\Install::getAdminFolder() . '/assets/img');
