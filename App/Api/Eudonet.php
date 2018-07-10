72fb8c9a9d234fd75237cae64cbbd91d48c343d0
Jammye <guillaume@jweb-creation.fr>
2018-07-10 11:28:55 +0200

    Ajout de Eudonet

---
+++ App/Api/Eudonet.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Api;
+
+use GuzzleHttp\Client;
+
+class Eudonet
+{
+    /* ************************************************** */
+    /* ****************   VARIABLES   ******************* */
+    /* ************************************************** */
+
+    private $token = '' ;
+    private $headers = [] ;
+    private static $instance = NULL ;
+
+    /* ************************************************** */
+    /* ****************   CONSTRUCT   ******************* */
+    /* ************************************************** */
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://xrm3.eudonet.com/EudoAPI/',
+        ]);
+    }
+
+    /* ************************************************** */
+    /* ****************     SETTER    ******************* */
+    /* ************************************************** */
+
+
+
+    /* ************************************************** */
+    /* ****************     GETTER    ******************* */
+    /* ************************************************** */
+
+
+
+    /* ************************************************** */
+    /* ****************    SINGLETON   ****************** */
+    /* ************************************************** */
+
+    public static function getInstance()
+    {
+        if ( self::$instance === NULL ) self::$instance = new Eudonet;
+        return self::$instance ;
+    }
+
+    /* ************************************************** */
+    /* ****************      TOKEN     ****************** */
+    /* ************************************************** */
+
+    private function getToken()
+    {
+        $rst = $this->request("post" , 'Authenticate/Token' , [
+            "SubscriberLogin" => EUDO_SUBSCRIBER_LOGIN,
+            "SubscriberPassword" => EUDO_SUBSCRIBER_PASSWORD,
+            "BaseName" => EUDO_BASE_NAME,
+            "UserLogin" => EUDO_USER_LOGIN,
+            "UserPassword" => EUDO_USER_PASSWORD,
+            "UserLang" => EUDO_USER_LANG,
+            "ProductName" => EUDO_PRODUCT_NAME
+        ]);
+
+        dump( $rst );
+    }
+
+    /* ************************************************** */
+    /* ****************     REQUEST    ****************** */
+    /* ************************************************** */
+
+    private function request( $type , $route , $params )
+    {
+        $type = strtolower( $type );
+        try
+        {
+            $authResponse = $this->client->$type( $route , $route . "authent                                                                                                                                                             icate" , [
+                'headers' => array_merge([
+                    'Content-Type' => "application/json" ,
+                ], $this->headers ),
+                'json' => $params
+            ] );
+
+            return json_decode($authResponse->getBody(), true);
+        } catch ( Exception $e )
+        {
+            dump( $e );
+        }
+
+    }
+}
