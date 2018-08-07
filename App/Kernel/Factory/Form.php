5b6df3c27892029fd463e3047023f759a73395a3
Jeremy Zunino <jeremy@jweb-creation.fr>
2018-08-07 10:39:18 +0200

    Factory Form : correction namespace appelé pour Custom

--- App/Kernel/Factory/Form.php
+++ App/Kernel/Factory/Form.php
@@ -229,7 +229,7 @@ class Form
 
         if ( file_exists( FORM_PROJECT_PATH . '/' . $className . '.php' ) )
         {
-            $className  = "\Project\Form\\" . $className ;
+            $className  = "\Project\Module\Form\\" . $className ;
             $classExist = true ;
         }
         else if ( file_exists( FORM_PATH . '/' . $className . '.php' ) )
