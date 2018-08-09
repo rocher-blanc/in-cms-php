8aa31bbb6f13455e442f6693a1b5765777bad7dc
Jammye <guillaume@jweb-creation.fr>
2018-08-09 11:04:24 +0200

    Ajout du timepicker

--- App/Kernel/Form/Date.php
+++ App/Kernel/Form/Date.php
@@ -6,11 +6,18 @@ class Date extends \App\Kernel\Back\Form
 {
 	public function html( $field, $name, $value = NULL )
 	{
-		$this->_lib_js  = [
+        $this->_lib_js  = [
             'cmsmedias/canvas/js/components/datepicker.js',
             'cmsmedias/canvas/js/components/datepicker.fr.js'
         ];
-		$this->_lib_css = 'cmsmedias/canvas/css/src/components/datepicker.css';
+
+        if ( $field->getData('hour') )
+        {
+            $this->_lib_js[] = 'cmsmedias/canvas/js/components/timepicker.js';
+            $this->_lib_css[] = 'cmsmedias/canvas/css/src/components/timepicker.css';
+        }
+
+        $this->_lib_css[] = 'cmsmedias/canvas/css/src/components/datepicker.css';
 
         return $this->View()->fetch( 'form/date.twig' , [
             'name' => $name,
