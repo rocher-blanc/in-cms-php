56f8fd1fe928c12adb10a75416dbf211c3fc259c
Jeremy Zunino <jeremy@jweb-creation.fr>
2018-08-31 09:34:40 +0200

    Correction bug BO : getOnIndex

--- App/Kernel/Back/Repository.php
+++ App/Kernel/Back/Repository.php
@@ -191,34 +191,6 @@ class Repository extends \App\Kernel\Common\Repository
         return $content ;
     }
 
-    public function getOnIndex( $id_module )
-    {
-        $tbl    = \DB::getTableName( $this->getName() );
-        $idName	= \DB::getIdName( $this->getName() ) ;
-
-        if ( ! $this->getEntity()->get( $this->getEntity()->getUrlName() )->hasLang() )   $tblField = \DB::getTableName( $this->getName() );
-        else                                                                              $tblField = \DB::getTableNameLang( $this->getName() );
-
-        $seo_module_one = \DB::for_module( $this->getName() )
-            ->select( 'seo.seo_title' )
-            ->select( 'seo.seo_description' )
-            ->select( $tbl . '.' . $idName , 'id' )
-            ->select( $tblField . '.' . $this->getEntity()->get( $this->getEntity()->getUrlName() )->getColumn() , 'value' )
-            ->left_outer_join( 'seo' , [ 'seo.seo_element_id' , '=', $tbl . '.' . $idName ] );
-
-        if ( $this->getEntity()->get( $this->getEntity()->getUrlName() )->hasLang() )
-        {
-            $seo_module_one->left_outer_join( $tblField , array( $tbl . '.' . $idName , '=', $tblField . '.' . \DB::getIdNameInLang( $this->getName() ) ))
-                ->where_equal($tblField . '.' . \DB::getLangIdLangName( $this->getName() ) , \App\Kernel\Lang::getInstance()->getDefault()->id);
-        }
-
-        $seo_module_one->where_equal('seo.seo_module_id', $id_module)
-            ->where_in('seo.seo_lang_id',\App\Kernel\Lang::getInstance()->getTabLang() )
-            ->where_raw("(seo.seo_title IS NULL OR seo.seo_description IS NULL)",[])
-            ->group_by('seo.seo_element_id')
-            ->find_many();
-    }
-
     public function getParentContent( $module_element_parent_id = NULL )
     {
         $all = \DB::for_module( $this->getName() );
