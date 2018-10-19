fb69ea5d546b8b9c86f8264a198f8bf43256f1ac
Jeremy Zunino <jeremy@jweb-creation.fr>
2018-10-19 10:19:03 +0200

    common.js : correction bug pour sites sans Canvas

--- assets/canvas/js/easydoor/common.js
+++ assets/canvas/js/easydoor/common.js
@@ -75,33 +75,35 @@ deleteElementExt = function( url ) {
 };
 
 headerResizing = function() {
-    var width = $("body").width();
-    var $notif = $("#primary-menu").find("#top-cart").eq(0);
-    var $ul = $("#primary-menu").find("ul").eq(0);
+    if( $("#primary-menu").find("ul").length > 0 ) {
+        var width = $("body").width();
+        var $notif = $("#primary-menu").find("#top-cart").eq(0);
+        var $ul = $("#primary-menu").find("ul").eq(0);
 
-    // Clean header
-    $ul.removeClass("small-space");
-    $ul.removeClass("two-lines");
-    $("#header").removeClass("static-sticky");
-    $notif.css("padding-right", $("#primary-menu .testimonial").outerWidth());
+        // Clean header
+        $ul.removeClass("small-space");
+        $ul.removeClass("two-lines");
+        $("#header").removeClass("static-sticky");
+        $notif.css("padding-right", $("#primary-menu .testimonial").outerWidth());
 
-    // Big screen
-    if( width > 992 ) {
-        var firstTop = $ul.find("li").first().offset().top;
-        var lastTop = $ul.find("li").last().offset().top;
+        // Big screen
+        if( width > 992 ) {
+            var firstTop = $ul.find("li").first().offset().top;
+            var lastTop = $ul.find("li").last().offset().top;
 
-        // Small space test
-        if( firstTop !== lastTop ) {
-            $ul.addClass("small-space");
-
-            // Two lines test
-            firstTop = $ul.find("li").first().offset().top;
-            lastTop = $ul.find("li").last().offset().top;
+            // Small space test
             if( firstTop !== lastTop ) {
-                $ul.addClass("two-lines");
-                $("#header").addClass("static-sticky");
-            }
+                $ul.addClass("small-space");
 
+                // Two lines test
+                firstTop = $ul.find("li").first().offset().top;
+                lastTop = $ul.find("li").last().offset().top;
+                if( firstTop !== lastTop ) {
+                    $ul.addClass("two-lines");
+                    $("#header").addClass("static-sticky");
+                }
+
+            }
         }
     }
 };
