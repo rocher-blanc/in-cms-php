48f8215a340576c9f40dd23004098ad68d366d60
Jeremy Zunino <jeremy@jweb-creation.fr>
2018-11-13 11:49:46 +0100

    Easyletter Select Model + Modifications design Select multiple

--- App/Module/Entity/EdAutomationModel.php
+++ App/Module/Entity/EdAutomationModel.php
@@ -32,7 +32,45 @@ class EdAutomationModel extends Builder
         $this->build('vars')
             ->column(1, 3)
             ->isCheckbox()
-            ->ManyToMany('EdAutomationVar')
+            ->option( $this->getList_EdAutomationVar() )
             ->name("Variables d'environnements");
     }
+
+
+	private function getList_EdAutomationVar()
+	{
+		// Prepare modules and result
+		$mod_group = \App\Kernel\Container::getInstance()->module("EdAutomationVarGroup");
+		$mod_element = \App\Kernel\Container::getInstance()->module("EdAutomationVar");
+		$rst = [];
+
+		// Find all groups
+		$all_group = $mod_group->getRepository()->findAll();
+		if( $all_group )
+		{
+			// For each groups
+			foreach( $all_group as $row_group ) {
+				// Parse group and create self result
+				$group = $mod_group->getController()->parseValue($row_group);
+				$rst_temp = [];
+
+				// Find elements in group
+				$all_element = $mod_element->getRepository()->getKit()
+					->where_equal( $mod_element->getEntity()->get('element_module_parent_id')->getColumn(), $group['id'] )
+					->find_many();
+				if( $all_element )
+				{
+					foreach( $all_element as $row_element )
+					{
+						$element = $mod_element->getController()->parseValue($row_element);
+						$rst_temp[ $element['id'] ] = $element['label'];
+					}
+					// Add group to final result
+					$rst[ $group['name'] ] = $rst_temp;
+				}
+			}
+		}
+		// Return final result
+		return $rst;
+	}
 }
