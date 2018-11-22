1bce9d31ab7984b4c2debda51591f708e42129e7
Guillaume <guillaume@jweb-creation.fr>
2018-11-22 16:30:47 +0100

    update

--- App/Kernel/Back/Controller.php
+++ App/Kernel/Back/Controller.php
@@ -935,7 +935,7 @@ class Controller extends \App\Kernel\Common\Controller
                     if ( $add ) $hookAfterCheck = 'hookAddSaveAfter' ;
                     else        $hookAfterCheck = 'hookUpdateSaveAfter' ;
 
-                    $this->$hookAfterCheck();
+                    $this->$hookAfterCheck( $content );
                 }
                 else
                 {
