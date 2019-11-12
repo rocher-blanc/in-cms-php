2795a24db9e9750cabb545852d7588f3a31d177e
Jeremy Zunino <jeremy@jweb-creation.fr>
2019-11-12 09:53:26 +0100

    Select isParent : Correction de l'affichage des titres dans le select

--- App/Kernel/Form/Select.php
+++ App/Kernel/Form/Select.php
@@ -24,7 +24,8 @@ class Select extends \App\Kernel\Back\Form
         if ( $field->isParent() )
         {
             if ( ! $field->getData('noEmptyValue') ) $select = '<option value=""' . ( $value === NULL ? ' selected' : '' ) . '>---</option>' ;
-            $select.= $this->chieldParent( $field->getData('option') , $field->getData('target') , $value , "" ) ;
+//            $select.= $this->chieldParent( $field->getData('option') , $field->getData('target') , $value , "" ) ;
+            $select.= $this->chieldParent( $field->getData('option') , "titre" , $value , "" ) ;
         }
         else
         {
