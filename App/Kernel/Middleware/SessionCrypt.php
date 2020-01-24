16b57cd50481f6d6f9552b4775ab31baff04f837
Guillaume <guillaume@jweb-creation.fr>
2020-01-24 15:04:30 +0100

    Suppression de l'envoi des sessions via des cookies

--- App/Kernel/Middleware/SessionCrypt.php
+++ App/Kernel/Middleware/SessionCrypt.php
@@ -2,13 +2,11 @@
 
 namespace App\Kernel\Middleware;
 
-use App\Kernel\Exception;
-
 class SessionCrypt extends \Slim\Middleware\SessionCookie
 {
     protected function getKey()
     {
-       return substr( md5( base64_encode( $this->app->request()->getUrl() ) ) , 0 , 24 ) ;
+        return substr( md5( base64_encode( $this->app->request()->getUrl() ) ) , 0 , 24 ) ;
     }
 
     /**
@@ -17,18 +15,6 @@ class SessionCrypt extends \Slim\Middleware\SessionCookie
     protected function loadSession()
     {
         if ( session_id() === '' ) session_start() ;
-
-        $value = mcrypt_decrypt(MCRYPT_3DES, $this->getKey(), $this->app->getCookie($this->settings['name']), MCRYPT_MODE_ECB);
-
-        if ( $value ) {
-            try {
-                $_SESSION = unserialize( base64_decode( $value ) ) ;
-            } catch (\Exception $e) {
-                $this->app->getLog()->error('Error unserializing session cookie value! ' . $e->getMessage());
-            }
-        } else {
-            $_SESSION = [];
-        }
     }
 
     /**
@@ -36,20 +22,6 @@ class SessionCrypt extends \Slim\Middleware\SessionCookie
      */
     protected function saveSession()
     {
-        $value = mcrypt_encrypt(MCRYPT_3DES, $this->getKey(), base64_encode(serialize($_SESSION)), MCRYPT_MODE_ECB);
 
-        if (strlen($value) > 4096) {
-            $this->app->getLog()->error('WARNING! Slim\Middleware\SessionCookie data size is larger than 4KB. Content save failed.');
-        } else {
-            $this->app->setCookie(
-                $this->settings['name'],
-                $value,
-                ( time() + COOKIE_EXPIRES ), // $this->settings['expires']
-                $this->settings['path'],
-                $this->settings['domain'],
-                $this->settings['secure'],
-                $this->settings['httponly']
-            );
-        }
     }
 }
