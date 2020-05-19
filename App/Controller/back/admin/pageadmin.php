dc164d9788f0211f975f102f25bb8e006ee028d1
Jeremy <jeremy@29ter.fr>
2020-05-19 10:53:39 +0200

    BO : création des pages : correction du bug de création de page + ajout des commentaires

--- App/Controller/back/admin/pageadmin.php
+++ App/Controller/back/admin/pageadmin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Kernel\Front\Translate;
+use App\Kernel\Factory;
 
 $app->group('/pageadmin', function () use ($app)
 {
@@ -62,7 +63,7 @@ $app->group('/pageadmin', function () use ($app)
         ]);
 	})->name('page_index');
 
-	$app->map('/edit', function () use ($app)
+	$app->get('/edit', function () use ($app)
 	{
 		$error 	  = false ;
 		$tabError = array() ;
@@ -91,97 +92,6 @@ $app->group('/pageadmin', function () use ($app)
             }
         }
 
-		if ( $app->request->isPost() )
-		{
-		    $contentRow = \DB::for_table('page')->create();
-
-			$ct = \DB::for_table('page')
-                        ->where_equal('page_domain_id' , $app->request->post('page_domain_id'))
-                        ->count();
-
-			if ($ct == 0)
-			{
-				$contentRow->page_default = 1;
-				$forceActive = true;
-			}
-
-			if ($app->request->post('page_name') == "")
-			{
-				$error = true;
-				$tabError['page_name'] = Translate::getInstance()->getText( 'mandatory_fillin');
-			}
-
-			if ($error == false)
-			{
-				$contentRow->page_name = $app->request->post('page_name');
-                $contentRow->page_active = $app->request->post('page_active');
-                $contentRow->page_index = 1;
-                $contentRow->page_domain_id = ( $reqDomains ? $app->request->post('page_domain_id') : 0 );
-
-                if ( ACTIVE_USER )
-                {
-                    $contentRow->page_access_user = $app->request->post('page_access_user');
-                    $contentRow->page_access_user_group = serialize( $app->request->post('page_access_user_group') );
-                }
-				$contentRow->save();
-
-				\App\Kernel\Back\Log::getInstance()->info(33, $contentRow->page_name);
-
-				$id     = $contentRow->page_id;
-                $lang 	= \App\Kernel\Lang::getInstance()->getAll() ;
-
-                foreach( $lang as $l )
-                {
-                    $cLang = \DB::for_table('page_lang')->where(['page_lang_page_id' => $id, 'page_lang_lang_id' => $l->id])->find_one();
-
-                    if ( ! $cLang )
-                    {
-                        $cLang = \DB::for_table('page_lang')->create();
-                    }
-
-                    $Factory = \App\Kernel\Factory::getInstance() ;
-
-                    $url = $Factory->Url()->uniq( "page" . $id , $l->id ) ;
-
-                    $cLang->page_lang_page_id 		= $id ;
-                    $cLang->page_lang_lang_id 		= $l->id ;
-                    $cLang->page_lang_url 			= $url ;
-                    $cLang->page_lang_title 		= NULL ;
-                    $cLang->page_lang_description 	= NULL ;
-                    $cLang->save();
-                }
-
-				if ($app->request->post('submit') == "stay") $url = '/admin/pageadmin/edit/' . $id;
-				else                                         $url = '/admin/pageadmin';
-
-				/* ************ Création du controller PHP ************ */
-				$php = '' ;
-				$php.= "<"."?"."php\n\n" ;
-				$php.= "namespace Project\Controller\Front;\n\n" ;
-				$php.= "class Page" . $id . " extends \App\Kernel\Front\Page\n" ;
-				$php.= "{\n" ;
-				$php.= "\t\n" ;
-				$php.= "}" ;
-				if ( ! file_exists( PROJECT_PATH . "/Controller/Front/Page" . $id . ".php" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/Controller/Front/Page" . $id . ".php" , $php );
-
-				/* ************ Création du template Twig ************ */
-				$tpl = '{% extends \'base.twig\' %}' . "\n";
-				$tpl.= '{% block contenu %}Page ' . $id . '{% endblock %}' . "\n";
-				if ( ! file_exists( PROJECT_PATH . "/view/front/page/page-" . $id . ".twig" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/view/front/page/page-" . $id . ".twig" , $tpl );
-
-
-				/* ************ Création de base.twig.html s'il n'existe pas ************ */
-				$tpl = "{% extends 'layout.twig.html' %}\n" ;
-				$tpl.= '{% block content %}' . "\n";
-				$tpl.= "\t{{ block('contenu') }}\n" ;
-				$tpl.= '{% endblock %}' . "\n";
-				if ( ! file_exists( PROJECT_PATH . "/view/front/base.twig" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/view/front/base.twig" , $tpl );
-
-				$Factory = \App\Kernel\Factory::getInstance();
-				$Factory->Response()->flashAndRedirect(Translate::getInstance()->getText( 'msg_special_page_add'), true, $url);
-			}
-		}
-
         $groupRows = \DB::for_table('user_front_group')
             ->order_by_asc('user_front_group_name')
             ->find_many();
@@ -195,7 +105,151 @@ $app->group('/pageadmin', function () use ($app)
             "tabError"	 => json_encode( $tabError ),
     ));
 
-	})->name('page_add')->via('GET', 'POST');
+	})->name('page_add');
+
+	$app->post('/edit', function () use ($app)
+	{
+		$error 	  = false ;
+		$tabError = array() ;
+
+		$post = array(
+			"page_name"       => $app->request->post('page_name'),
+			"page_controller" => $app->request->post('page_controller'),
+			"page_active"     => $app->request->post('page_active')
+		);
+
+        $domains = [];
+        $reqDomains = \DB::for_table('domain')
+            ->select('domain_id')
+            ->select('domain_name')
+            ->find_many();
+
+        if( $reqDomains )
+        {
+            foreach( $reqDomains as $row )
+            {
+                $domains[] = [
+                    'id'   => $row->domain_id,
+                    'name' => $row->domain_name,
+                    'selected' => false
+                ];
+            }
+        }
+
+		$contentRow = \DB::for_table('page')->create();
+
+		$ct = \DB::for_table('page')
+					->where_equal('page_domain_id' , $app->request->post('page_domain_id'))
+					->count();
+
+		if ($ct == 0)
+		{
+			$contentRow->page_default = 1;
+			$forceActive = true;
+		}
+
+		if ($app->request->post('page_name') == "")
+		{
+			$error = true;
+			$tabError['page_name'] = Translate::getInstance()->getText( 'mandatory_fillin');
+		}
+
+		if ($error == false)
+		{
+			$pageName = $app->request->post('page_name');
+
+			$contentRow->page_name = $pageName;
+			$contentRow->page_active = $app->request->post('page_active');
+			$contentRow->page_index = 1;
+			$contentRow->page_domain_id = ( $reqDomains ? $app->request->post('page_domain_id') : 0 );
+
+			if ( ACTIVE_USER )
+			{
+				$contentRow->page_access_user = $app->request->post('page_access_user');
+				$contentRow->page_access_user_group = serialize( $app->request->post('page_access_user_group') );
+			}
+			$contentRow->save();
+
+			\App\Kernel\Back\Log::getInstance()->info(33, $contentRow->page_name);
+
+			$id     = $contentRow->page_id;
+			$lang 	= \App\Kernel\Lang::getInstance()->getAll() ;
+
+			foreach( $lang as $l )
+			{
+				$cLang = \DB::for_table('page_lang')->where(['page_lang_page_id' => $id, 'page_lang_lang_id' => $l->id])->find_one();
+
+				if ( ! $cLang )
+				{
+					$cLang = \DB::for_table('page_lang')->create();
+				}
+
+				$Factory = \App\Kernel\Factory::getInstance() ;
+
+				$url = $Factory->Url()->uniq( "page" . $id , $l->id ) ;
+
+				$cLang->page_lang_page_id 		= $id ;
+				$cLang->page_lang_lang_id 		= $l->id ;
+				$cLang->page_lang_url 			= $url ;
+				$cLang->page_lang_title 		= NULL ;
+				$cLang->page_lang_description 	= NULL ;
+				$cLang->save();
+			}
+
+			if ($app->request->post('submit') == "stay") $url = '/admin/pageadmin/edit/' . $id;
+			else                                         $url = '/admin/pageadmin';
+
+			/* ************ Création du controller PHP ************ */
+			$php = '' ;
+			$php.= "<"."?"."php\n\n" ;
+			$php.= "namespace Project\Controller\Front;\n\n" ;
+			$php.= "/* Page $id : $pageName */\n" ;
+			$php.= "class Page" . $id . " extends \App\Kernel\Front\Page\n" ;
+			$php.= "{\n" ;
+			$php.= "\t\n" ;
+			$php.= "}" ;
+			if ( ! file_exists( PROJECT_PATH . "/Controller/Front/Page" . $id . ".php" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/Controller/Front/Page" . $id . ".php" , $php );
+
+			/* ************ Création du template Twig ************ */
+			$tpl = "{# Page $id : $pageName #}\n";
+			$tpl.= '{% extends \'base.twig\' %}' . "\n";
+			$tpl.= '{% block contenu %}Page ' . $id . '{% endblock %}' . "\n";
+			if ( ! file_exists( PROJECT_PATH . "/view/front/page/page-" . $id . ".twig" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/view/front/page/page-" . $id . ".twig" , $tpl );
+
+
+			/* ************ Création de base.twig.html s'il n'existe pas ************ */
+			$tpl = "{% extends 'layout.twig.html' %}\n" ;
+			$tpl.= '{% block content %}' . "\n";
+			$tpl.= "\t{{ block('contenu') }}\n" ;
+			$tpl.= '{% endblock %}' . "\n";
+			if ( ! file_exists( PROJECT_PATH . "/view/front/base.twig" ) ) \App\Kernel\Factory::getInstance()->File()->create( PROJECT_PATH . "/view/front/base.twig" , $tpl );
+
+//			$Factory = \App\Kernel\Factory::getInstance();
+//			$Factory->Response()->flashAndRedirect(Translate::getInstance()->getText( 'msg_special_page_add'), true, $url);
+		}
+
+        $groupRows = \DB::for_table('user_front_group')
+            ->order_by_asc('user_front_group_name')
+            ->find_many();
+
+		$result = [];
+		if( count($tabError) === 0 )
+		{
+			if ( $app->request->post('buttonaction') == "stay" ) 	$url = '/admin/pageadmin/edit/' . $id ;
+			else 										         	$url = '/admin/pageadmin' ;
+
+			$result['result'] = true ;
+			$result['url']   = Factory::getInstance()->Url()->get( $url ) ;
+			$result['msg']   = Translate::getInstance()->getText( 'msg_special_page_add');
+		}
+		else
+		{
+			$result['result'] = false ;
+			$result['msg']    = current( $tabError );
+		}
+		Factory::getInstance()->Response()->printJSON($result) ;
+
+	})->name('page_add');
 
     $app->get('/edit/:id', function ( $id ) use ($app)
     {
