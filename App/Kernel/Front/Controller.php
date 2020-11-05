cd5c273ce34f2027a4e8a8fea8ba9998a32529fa
Guillaume <guillaume@jweb-creation.fr>
2020-11-05 17:35:41 +0100

    bug fix - checkbox

--- App/Kernel/Front/Controller.php
+++ App/Kernel/Front/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Kernel\Front;
 
 use App\Kernel\Back\Seo;
+use App\Kernel\Entity\Field;
 use App\Kernel\Exception;
 use App\Kernel\Front\Alt;
 use App\Kernel\Front\Gallery;
@@ -1215,6 +1216,9 @@ class Controller extends \App\Kernel\Common\Controller
                                 $content = $this->getRepository()->create();
                             }
 
+                            /**
+                             * @var Field $row
+                             */
                             foreach( $this->getEntity()->getField() as $row )
                             {
                                 if ( $this->checkCustomField( $row ) == true )
@@ -1268,9 +1272,12 @@ class Controller extends \App\Kernel\Common\Controller
 
                             if ( $this->getId() === NULL ) $this->setId( $content->get( $this->getEntity()->get( $this->getEntity()->getIdName() )->getColumn() ) );
 
+                            /**
+                             * @var Field $field
+                             */
                             foreach( $this->getEntity()->getField() as $nameField => $field )
                             {
-                                if ( $field->getType() == "checkbox" )
+                                if ( $field->getType() == "checkbox" && $field->front() !== false )
                                 {
                                     $this->getRepository()->pushDataAssoc($nameField, $field, $this->getId());
                                 }
