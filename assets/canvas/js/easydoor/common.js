040ac9628379c78ea13f48c632117584be8e3e41
Jeremy <jeremy@29ter.fr>
2020-12-15 10:39:32 +0100

    BO : Correction select2

--- assets/canvas/js/easydoor/common.js
+++ assets/canvas/js/easydoor/common.js
@@ -43,6 +43,40 @@ redirect = function( url ) {
     d.location.href = url ;
 };
 
+select2MatchStart = function(params, data) {
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+        return data;
+    }
+
+    // Skip if there is no 'children' property
+    if (typeof data.children === 'undefined') {
+        return null;
+    }
+
+    // `data.children` contains the actual options that we are matching against
+    var filteredChildren = [];
+    $.each(data.children, function (idx, child) {
+        if (child.text.toUpperCase().indexOf(params.term.toUpperCase()) == 0) {
+            filteredChildren.push(child);
+        }
+    });
+
+    // If we matched any of the timezone group's children, then set the matched children on the group
+    // and return the group object
+    if (filteredChildren.length) {
+        var modifiedData = $.extend({}, data, true);
+        modifiedData.children = filteredChildren;
+
+        // You can return modified objects from here
+        // This includes matching the `children` how you want in nested data sets
+        return modifiedData;
+    }
+
+    // Return `null` if the term should not be displayed
+    return null;
+};
+
 initSelect = function( base ) {
     if ( $(base + " select[data-plugin-selectTwo]").length ) {
         $(base + " select[data-plugin-selectTwo]").select2({
@@ -50,7 +84,8 @@ initSelect = function( base ) {
                 "noResults": function () {
                     return "Aucun résultat trouvé";
                 }
-            }
+            },
+            matcher: select2MatchStart
         });
     }
 };
