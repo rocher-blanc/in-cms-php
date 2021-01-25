4fbb35dfe2ab4eba4c9e04cd7e72024532632bdf
Jeremy <jeremy@29ter.fr>
2021-01-25 10:24:13 +0100

    Correction du date picker

--- App/Kernel/Form/Date.php
+++ App/Kernel/Form/Date.php
@@ -23,7 +23,6 @@ class Date extends \App\Kernel\Back\Form
             'name' => $name,
             'value' => $value,
             'hour' => $field->getData('hour'),
-            'old' => $field->getData('old'),
             'column' => $field->getColumn(),
             'required' => $field->isRequired()
         ]);
