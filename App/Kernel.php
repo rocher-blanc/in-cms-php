9fa6814648b36dd949b4861dbe7b4d05982f9051
Guillaume <guillaume@29ter.fr>
2021-07-23 10:56:56 +0200

    Correction naming

--- App/Kernel.php
+++ App/Kernel.php
@@ -63,7 +63,7 @@ class Kernel
 
     public function load()
     {
-        defined('APPLICATION_PATH') || define('APPLICATION_PATH', VENDOR_PATH . '/JWebCreation/cms/App');
+        defined('APPLICATION_PATH') || define('APPLICATION_PATH', VENDOR_PATH . '/jwebcreation/cms/App');
 
         require APPLICATION_PATH . '/config/config.php';
         require APPLICATION_PATH . '/config/config.' . $this->config('config') . '.php';
