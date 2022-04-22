6d37bb83029a3a198eecd3725ce76e06e23442c9
Duret Kevin <duretkevin@live.fr>
2022-04-22 14:55:05 +0200

    Update Url.php

--- App/Kernel/Factory/Url.php
+++ App/Kernel/Factory/Url.php
@@ -241,8 +241,6 @@ class Url
             }
         }
 
-//        if ( $urlFull ){ dump($url); die; }
-
         if ( $cLang ) 	return ( $urlFull ? $url : '' ) . ( \App\Kernel\Lang::getInstance()->count() > 1 ? \App\Kernel\Lang::getInstance()->getActive()->url . "/" : '' ) . ( $cLang->page_default == 1 ? '' : $cLang->page_lang_url ) ;
         else			return "#" ;
     }
